ff5090eb3ae70891430674530ffcfcfa42b60a84
George <koladegeore5@gmail.com>
2025-05-01 03:37:46 +0100

    doctors pages update

--- hospapp/urls.py
+++ hospapp/urls.py
@@ -4,8 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.hms_admin, name='hms_admin'),
-    path('nurses', views.nurses, name='nurse'),
+    path('n/home', views.nurses, name='nurse'),
+
     path('admission', views.admission_discharge_view, name='admission_discharge'),
     path('bed-ward-management', views.bed_ward_management_view, name='bed_ward_management'),
     path('vitals', views.vitals, name='vitals'),
@@ -15,14 +15,16 @@ urlpatterns = [
     path('task_assignments', views.task_assignments_view, name='task_assignments'),
     path('emergency_alerts', views.emergency_alerts_view, name='emergency_alerts'),
 
-    path('doctors/', views.doctors, name='doctors'),
-    path('patient_list', views.patient_list, name='patient_list'),
-    path('doctors/patient-list/', views.patient_list, name='patient_list'),
-    path('doctor/view/<int:patient_id>/', views.view_patient, name='view_patient'),
-    path('doctor/diagnose/<int:patient_id>/', views.add_diagnosis, name='add_diagnosis'),
-    path('doctor/prescribe/<int:patient_id>/', views.prescribe_med, name='prescribe_med'),
-    path('doctor/notes/<int:patient_id>/', views.write_notes, name='write_notes'),
-    path('doctor/medical-records/', views.access_medical_records, name='access_medical_records'),
+    path('d/home', views.doctors, name='doctors'),
+    path('d/consultations', views.doctor_consultation, name='doctor_consultation'),
+    path('d/medical-records/', views.access_medical_records, name='access_medical_records'),
+    path('d/monitoring/', views.monitoring, name='monitoring'),
+    # path('patient_list', views.patient_list, name='patient_list'),
+    # path('doctors/patient-list/', views.patient_list, name='patient_list'),
+    # path('doctor/view/<int:patient_id>/', views.view_patient, name='view_patient'),
+    # path('doctor/diagnose/<int:patient_id>/', views.add_diagnosis, name='add_diagnosis'),
+    # path('doctor/prescribe/<int:patient_id>/', views.prescribe_med, name='prescribe_med'),
+    # path('doctor/notes/<int:patient_id>/', views.write_notes, name='write_notes'),
 
     path('ae/', views.ae, name='ae'),
     path('laboratory/', views.laboratory, name='laboratory'),
@@ -32,6 +34,6 @@ urlpatterns = [
     path('inventory/', views.inventory, name='inventory'),
     path('hms_admin', views.hms_admin, name='hms_admin'),
 
-    path('r/dashboard', views.receptionist, name='receptionist'),
+    path('r/home', views.receptionist, name='receptionist'),
     path('r/new_patient', views.register_patient, name='register_patient'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- hospapp/views.py
+++ hospapp/views.py
@@ -164,63 +164,66 @@ def emergency_alerts_view(request):
 def doctors(request):
     return render(request, 'doctors/index.html')
 
-def patient_list(request):
-    return render(request, 'doctors/patient_list.html')
+def doctor_consultation(request):
+    return render(request, 'doctors/consultation.html')
+
+# def patient_list(request):
+#     return render(request, 'doctors/patient_list.html')
 
 # View for the Patient List Page
-def patient_list(request):
-    patients = Patient.objects.all()
-    inpatients = patients.filter(is_inpatient=True)
-    outpatients = patients.filter(is_inpatient=False)
-    critical_count = patients.filter(status="critical").count()
+# def patient_list(request):
+#     patients = Patient.objects.all()
+#     inpatients = patients.filter(is_inpatient=True)
+#     outpatients = patients.filter(is_inpatient=False)
+#     critical_count = patients.filter(status="critical").count()
 
-    context = {
-        'patients': patients,
-        'inpatients': inpatients,
-        'outpatients': outpatients,
-        'critical_count': critical_count
-    }
-    return render(request, 'doctors/patient_list.html', context)
+#     context = {
+#         'patients': patients,
+#         'inpatients': inpatients,
+#         'outpatients': outpatients,
+#         'critical_count': critical_count
+#     }
+#     return render(request, 'doctors/patient_list.html', context)
 
-# View for Viewing Patient Profile
-def view_patient(request, patient_id):
-    patient = get_object_or_404(Patient, id=patient_id)
-    return render(request, 'doctors/view_patient.html', {'patient': patient})
+# # View for Viewing Patient Profile
+# def view_patient(request, patient_id):
+#     patient = get_object_or_404(Patient, id=patient_id)
+#     return render(request, 'doctors/view_patient.html', {'patient': patient})
 
-# View for Adding Diagnosis
-def add_diagnosis(request, patient_id):
-    patient = get_object_or_404(Patient, id=patient_id)
-    if request.method == 'POST':
-        # Logic for adding diagnosis here
-        # You can add diagnosis to the patient
-        diagnosis = request.POST.get('diagnosis')
-        patient.diagnosis = diagnosis
-        patient.save()
-        return HttpResponseRedirect(reverse('view_patient', args=[patient.id]))
-    return render(request, 'doctors/add_diagnosis.html', {'patient': patient})
+# # View for Adding Diagnosis
+# def add_diagnosis(request, patient_id):
+#     patient = get_object_or_404(Patient, id=patient_id)
+#     if request.method == 'POST':
+#         # Logic for adding diagnosis here
+#         # You can add diagnosis to the patient
+#         diagnosis = request.POST.get('diagnosis')
+#         patient.diagnosis = diagnosis
+#         patient.save()
+#         return HttpResponseRedirect(reverse('view_patient', args=[patient.id]))
+#     return render(request, 'doctors/add_diagnosis.html', {'patient': patient})
 
-# View for Prescribing Medication
-def prescribe_med(request, patient_id):
-    patient = get_object_or_404(Patient, id=patient_id)
-    if request.method == 'POST':
-        # Logic for prescribing medication here
-        medication = request.POST.get('medication')
-        # Assume you have a Medication model to save prescriptions
-        patient.medication = medication
-        patient.save()
-        return HttpResponseRedirect(reverse('view_patient', args=[patient.id]))
-    return render(request, 'doctors/prescribe_medication.html', {'patient': patient})
+# # View for Prescribing Medication
+# def prescribe_med(request, patient_id):
+#     patient = get_object_or_404(Patient, id=patient_id)
+#     if request.method == 'POST':
+#         # Logic for prescribing medication here
+#         medication = request.POST.get('medication')
+#         # Assume you have a Medication model to save prescriptions
+#         patient.medication = medication
+#         patient.save()
+#         return HttpResponseRedirect(reverse('view_patient', args=[patient.id]))
+#     return render(request, 'doctors/prescribe_medication.html', {'patient': patient})
 
-# View for Writing Notes
-def write_notes(request, patient_id):
-    patient = get_object_or_404(Patient, id=patient_id)
-    if request.method == 'POST':
-        # Logic for adding notes here
-        notes = request.POST.get('notes')
-        patient.notes = notes
-        patient.save()
-        return HttpResponseRedirect(reverse('view_patient', args=[patient.id]))
-    return render(request, 'doctors/write_notes.html', {'patient': patient})
+# # View for Writing Notes
+# def write_notes(request, patient_id):
+#     patient = get_object_or_404(Patient, id=patient_id)
+#     if request.method == 'POST':
+#         # Logic for adding notes here
+#         notes = request.POST.get('notes')
+#         patient.notes = notes
+#         patient.save()
+#         return HttpResponseRedirect(reverse('view_patient', args=[patient.id]))
+#     return render(request, 'doctors/write_notes.html', {'patient': patient})
 
 # @login_required
 def access_medical_records(request):
@@ -228,6 +231,9 @@ def access_medical_records(request):
     return render(request, 'doctors/access_medical_records.html', {
         'patients': patients
     })
+
+def monitoring(request):
+    return render(request, 'doctors/treatment_monitoring.html')
                           
 def ae(request):     
     return render(request, 'ae/base.html')
